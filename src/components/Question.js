47b48e7202b3af07df03d706b7cd5d64fcbfae88
Mohammed <khamismitch@gmail.com>
2023-11-28 16:32:04 +0300

    complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,42 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  const [isActive, setIsActive] = useState(true);
 
   // add useEffect code
+  useEffect(() => {
+    // If isActive is true, set up the timer
+    if (isActive) {
+      const timerId = setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+
+      // Cleanup the timer when component unmounts or when timeRemaining hits 0
+      return () => {
+        clearTimeout(timerId);
+      };
+    }
+
+    // If timeRemaining hits 0, reset it to 10 and trigger onAnswered with false
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, isActive, onAnswered]);
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setIsActive(false);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
+
 
   const { id, prompt, answers, correctIndex } = question;
 
